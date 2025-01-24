9a32489fc9032655def29671f41d7e1c26db7558
RitaYujiaWu <yuw172@ucsd.edu>
2025-01-23 20:37:30 -0800

    step 3 done

--- global.js
+++ global.js
@@ -3,11 +3,47 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-const navLinks = $$("nav a")
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
-if (currentLink) {
-// or if (currentLink !== undefined)
-currentLink.classList.add('current');
+
+// const navLinks = $$("nav a")
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume/CV' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/RitaYujiaWu', title: 'GitHub Profile' }
+];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+
+    nav.append(a);
 }
+
+// nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+
+
+
+
