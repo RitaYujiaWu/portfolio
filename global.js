fb266cddb7475c990e785f5c51fc0b259a1f95b6
RitaYujiaWu <yuw172@ucsd.edu>
2025-01-31 13:49:39 -0800

    lab 4 done

--- global.js
+++ global.js
@@ -79,3 +79,35 @@ form?.addEventListener("submit", function (event) {
     location.href = url;
 });
 
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        console.log(response);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+    projects.forEach((project, index) => {
+        console.log(`Rendering project #${index}:`, project);
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <${headingLevel}>${project.title}</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>`;
+        
+        // Append the article to the container
+        containerElement.appendChild(article);
+    });
+}
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+  }
