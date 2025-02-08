7c9156d4bcf66277160887d178376a1e922154e7
RitaYujiaWu <yuw172@ucsd.edu>
2025-02-07 21:54:43 -0800

    lab 5 step 4 done

--- projects/projects.js
+++ projects/projects.js
@@ -25,29 +25,51 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 // }
 
 // let arcs = arcData.map((d) => arcGenerator(d));
+function renderPieChart(projectsGiven) {
 
-let rolledData = d3.rollups(
-    projects,
-    (v) => v.length,
-    (d) => d.year,
-);
+    let rolledData = d3.rollups(
+        projectsGiven,
+        (v) => v.length,
+        (d) => d.year,
+    );
 
-let data = rolledData.map(([year, count]) => {
-    return { value: count, label: year };
+    let data = rolledData.map(([year, count]) => {
+        return { value: count, label: year };
+    });
+
+    let sliceGenerator = d3.pie().value((d) => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map((d) => arcGenerator(d));
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    
+    d3.select('svg').selectAll('path').remove();
+    
+    arcs.forEach((arc, idx) => {
+        d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx))
+    })
+
+    d3.select('.legend').selectAll('li').remove();
+    let legend = d3.select('.legend');
+    data.forEach((d, idx) => {
+        legend.append('li')
+            .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+            .attr('class', 'legend-item')
+            .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+    })}
+
+renderPieChart(projects);
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('input', (event) => {
+    // update query value
+    query = event.target.value;
+    // filter projects
+    let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());});
+
+    // re-render legends and pie chart when event triggers
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+    renderPieChart(filteredProjects);
 });
-
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-arcs.forEach((arc, idx) => {
-    d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx))
-})
-
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .attr('class', 'legend-item')
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
-})
