6706e199e3764f9b4ff135d68f642d28813afe35
RitaYujiaWu <yuw172@ucsd.edu>
2025-02-07 20:23:09 -0800

    lab 5 step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -6,3 +7,28 @@ const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
 renderProjects(projects, projectsContainer, 'h2');
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+// let data = [1, 2];
+// let total = 0;
+
+// for (let d of data) {total += d;}
+
+// let angle = 0;
+// let arcData = [];
+
+// for (let d of data) {
+//   let endAngle = angle + (d / total) * 2 * Math.PI;
+//   arcData.push({ startAngle: angle, endAngle });
+//   angle = endAngle;
+// }
+
+// let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc, idx) => {
+    d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx))
+})
