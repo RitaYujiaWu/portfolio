7ca6391780dd961e21ce38c1e539b7c36d2a6fb6
RitaYujiaWu <yuw172@ucsd.edu>
2025-02-13 22:01:38 -0800

    step 4 done

--- meta/main.js
+++ meta/main.js
@@ -156,7 +156,7 @@ function createScatterPlot(){
     .append('g')
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
-    
+
     const dots = svg.append('g').attr('class', 'dots');
 
     dots
@@ -166,5 +166,35 @@ function createScatterPlot(){
     .attr('cx', (d) => xScale(d.datetime))
     .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
-    .attr('fill', 'steelblue');
+    .attr('fill', 'steelblue')
+    .on('mouseenter', (event, commit) => {
+    updateTooltipContent(commit);
+    updateTooltipVisibility(true);
+    updateTooltipPosition(event);})
+    .on('mouseleave', () => {
+    updateTooltipContent({});
+    updateTooltipVisibility(false);});
+}
+
+function updateTooltipContent(commit) {
+    const link = document.getElementById('commit-link');
+    const date = document.getElementById('commit-date');
+    
+    if (Object.keys(commit).length === 0) return;
+    
+    link.href = commit.url;
+    link.textContent = commit.id;
+    date.textContent = commit.datetime?.toLocaleString('en', {
+    dateStyle: 'full',});
+}
+
+function updateTooltipVisibility(isVisible) {
+    const tooltip = document.getElementById('commit-tooltip');
+    tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+    const tooltip = document.getElementById('commit-tooltip');
+    tooltip.style.left = `${event.clientX}px`;
+    tooltip.style.top = `${event.clientY}px`;
 }
